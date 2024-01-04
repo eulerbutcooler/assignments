61a3c1469f9a35957635227b7c90a6dc50421099
Amaan Pathan <eulerbutcooler@gmail.com>
2024-01-04 22:16:36 +0530

    added jwt to the course selling rough backened server

--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,26 +1,81 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { Admin, User, Course } = require("../db");
+const {JWT_SECRET} = require("../config");
+const jwt = require("jsonwebtoken");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post('/signup', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.create({
+        username: username,
+        password: password
+    })
+
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post('/signin', async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    console.log(JWT_SECRET);
+
+    const user = await User.find({
+        username,
+        password
+    })
+
+    if (user) {
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(411).json({
+            message: "Incorrect email and pass"
+        })
+    }
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get('/courses', async (req, res) => {
+    try{
+        const courses = await Course.find();
+        res.json(courses);
+    }
+    catch(err){
+        res.status(500).json({ message: 'Error fetching courses' });
+    }
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
+    try{
+        const course = await Course.findById(req.params.courseId);
+        if(!course){
+            res.status(404).json({ message: 'Course not found' });
+        }
+        res.json(course);
+    } catch(err){
+        res.status(500).json({ message: 'Error fetching course' });
+    }
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).populate('purchasedCourses');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user.purchasedCourses);
+    } catch (err) {
+        res.status(500).json({ message: 'Error fetching purchased courses' });
+    }
 });
 
 module.exports = router
